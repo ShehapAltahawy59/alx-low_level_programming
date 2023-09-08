63d5d8d110b54995bfae704ec9245e87bac54a7f
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 13:04:44 -0700

    edit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Retyrn: Always 0 (success)
+ */
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    }
+    putchar('\n');
+
+    return (0);
+}
