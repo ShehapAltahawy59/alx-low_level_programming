3f4740559f4f48fdc61b1a8ca24547a820bf63b2
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 13:34:06 -0700

    edit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	
+	putchar(',');
+
+	putchar(' ');
+
+	}
+	putchar('\n');
+
+	return (0);
+}
+
