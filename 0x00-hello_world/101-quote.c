4e94791232e7e254c8f66f3d607cb22c1f0e53c4
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 12:22:43 -0700

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- *ses the write function from the unistd.h library to write the specified string
+ *ses the write function from the unistd.h library
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 
