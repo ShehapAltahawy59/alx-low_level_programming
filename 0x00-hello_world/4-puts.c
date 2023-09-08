74b9f06639c74fd2fa7eaa85bf337ccbca8192f5
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 12:00:18 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
-/*print code using put*/
+/*print code using put*
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
