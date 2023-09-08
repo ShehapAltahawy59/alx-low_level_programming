8b42439c8ac5a817924f184b2606324254accca7
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 13:28:11 -0700

    edit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 16; number++)
+	{
+		if (number < 10)
+			putchar(number + '0');
+		else
+			putchar(number - 10 + 'a');
+	}
+	putchar('\n');
+
+	return (0);
+}
