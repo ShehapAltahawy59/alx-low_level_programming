c554558a5bdc78d8513d840cd97d2b46fd1442f5
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 12:33:34 -0700

    edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-
-/**
- * main - printf()
- *
- * Return: Always 0 (Seccess)
- */ 
+#include <stdio.h> 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
