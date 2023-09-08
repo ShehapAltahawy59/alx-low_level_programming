055b0d6242304a62f5efba35ed62fce641b25881
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 12:39:44 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*print code using put*/
+/**
+ * main - print code using put
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
