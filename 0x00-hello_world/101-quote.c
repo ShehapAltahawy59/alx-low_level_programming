3767573b784c8b81ed605b540fc74ed1064e2378
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-08 12:20:14 -0700

    edit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ *ses the write function from the unistd.h library to write the specified string
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
