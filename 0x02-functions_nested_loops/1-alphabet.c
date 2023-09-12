e142a52fc71a13d6ae9c51c0d5cabc7764a24eb8
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-12 09:52:52 -0700

    edit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@ void print_alphabet(void);
 
 int main(void)
 {
-	print_alphabet()
+	print_alphabet();
 	return (0)
 }
 /**
@@ -19,7 +19,7 @@ void print_alphabet(void)
 	char c;
 	for (c='a';c<='z';c++)
 	{
-		putchar(c)
+		putchar(c);
 	}
-	putchar('\n')
+	putchar('\n');
 }
