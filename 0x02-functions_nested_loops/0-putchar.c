7a442be77a76303d54578a52c607efbf956f0e9f
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-12 09:31:27 -0700

    edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,25 @@
  *
  * Return: 0
  */
+
+
+#include <stdio.h>
+
+// function prototype
+void print_putchar();
+
 int main() {
-    char str[] = "_putchar";
-    int i = 0;
-    while (str[i]) {
-        putchar(str[i]);
-        i++;
-    }
-    putchar('\n');
-    return 0;
+	print_putchar();
+	return 0;
+}
+
+// function definition
+void print_putchar() {
+	char str[] = "_putchar";
+	int i = 0;
+	while (str[i]) {
+		putchar(str[i]);
+		i++;
+		}
+		putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// includes
+#include <stdio.h>
+
+// function declarations
+void print_putchar();
+
+#endif
+
