0987aaf5456bf62553a1155e7b1b0c8700f9179c
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-13 01:33:26 -0700

    edit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	long int next, a = 1, b = 2, sum = 0;
+	
+	while (b <= 4000000)
+	{
+		if (b % 2 == 0)
+		{
+			sum += b;
+		}
+		next = a + b;
+		a = b;
+		b = next;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
