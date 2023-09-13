887a84476a2ff43bc458f2b17a1cc1bbcab8d50a
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-13 01:37:38 -0700

    edit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{	int i;
+	unsigned long int a = 1, b = 2, next;
+	
+	printf("%lu, %lu", a, b);
+	for (i = 3; i <= 98; i++)
+	{
+		next = a + b;
+		printf(", %lu", next);
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
+}
