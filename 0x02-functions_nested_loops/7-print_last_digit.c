b769a3f169f2728f94bcdba501e263be4b9a7926
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-13 00:20:09 -0700

    edit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_last_digit - print last digit of number
  * @n: the number used
@@ -10,6 +11,7 @@ int print_last_digit(int n)
 
 	if( last_digit < 0 )
 	{
+		_putchar('0'+last_digit);
 		last_digit=-last_digit;
 	}
 	return (last_digit);
