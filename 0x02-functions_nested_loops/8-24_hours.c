953cbda9597fa726c4263a78f9753e8a81eb9c93
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-13 00:36:45 -0700

    edit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - print time
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour,minute;
+
+	for (hour = 0; hour < 24;hour++)
+	{
+		h1= '0' + hour / 10;
+		h2= '0' + hour % 10;
+		for(minute = 0;minute < 60;minute++)
+		{
+			m1 = '0' + minute / 10;
+			m2 = '0' + minute % 10;
+			_putchar(h1);
+			_putchar(h2);
+			_putchar(m1);
+			_putchar(m2);
+			_putchar('\n');
+		}
+	}
+}
