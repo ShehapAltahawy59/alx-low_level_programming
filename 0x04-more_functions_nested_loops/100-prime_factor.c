2cdf37155d385525352093c0ea1c4d1674169941
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-15 12:11:35 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ * Return: zero
+ */
+int main(void)
+{
+	long n = 612852475143;
+	long max_prime = -1;
+	int i;
+	
+	while (n % 2 == 0)
+	{
+		max_prime = 2;
+		n >>= 1; /* equivalent to n /= 2 */
+	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			max_prime = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	max_prime = n;
+	printf("%ld\n", max_prime);
+	return (0);
+}
