34fb817879eacebae29c996a71117a3b934f3c06
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-18 00:36:57 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print rev
+ * @s: string
+ * Return: none
+ */
+
+void print_rev(char *s)
+{
+	char *pointer = s;
+	int size=0;
+
+	while (*pointer != '\0')
+	{
+		size++;
+		pointer++;
+	}
+	pointer--;
+	while (size != 0)
+	{
+		_putchar(*pointer);
+		size--;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,4 +11,6 @@ int _putchar(char c);
 
 void puts2(char *str);
 
+void print_rev(char *s);
+
 #endif /* MAIN_H */
