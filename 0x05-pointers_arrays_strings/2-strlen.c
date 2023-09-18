01dc81863a33d4141450abbe848c2ea760afa6a8
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-18 00:18:12 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - get lenght of string
+ * @s: pointer to string 
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	char pointer = s;
+	int len = 0;
+
+	if (*pointer == "\0")
+	{
+		return (len);
+	}
+	else
+	{
+		len++;
+		pointer = pointer+1;
+	}
+}
