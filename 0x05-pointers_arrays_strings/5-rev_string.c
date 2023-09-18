3ea13ce6b23d5228568a2ebd7de69a589930124a
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-18 00:48:27 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - rev string
+ * @s: string
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	char *pointer1 = s;
+	char *pointer2 = s;
+	char swap;
+
+	while (*pointer2 != '\0')
+	{
+		pointer2++;
+	}
+	pointer2--;
+	while (pointer1 != pointer2)
+	{
+		swap = *pointer2;
+		*pointer2 = *pointer1;
+		*pointer1 = swap;
+		pointer1++;
+		pointer2--;
+	}
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ void puts2(char *str);
 
 void print_rev(char *s);
 
+void rev_string(char *s);
+
 #endif /* MAIN_H */
