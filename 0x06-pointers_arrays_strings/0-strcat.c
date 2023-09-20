38ca070b9d7c9a5143f01d2f55bd74667cdae5c1
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-20 00:30:52 -0700

    edit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatinate string
+ * @dest: final string
+ * @src: first string
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+	
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
