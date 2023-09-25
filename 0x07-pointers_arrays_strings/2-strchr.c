5be96971b9bcd3572eb75d2cbb5aca38dd8ea219
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-25 00:39:47 -0700

    edit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to check for
+ *
+ * Return: pointer to spot in s with c or null
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		if (*p == c)
+		{
+			return (p);
+		}
+		p++;
+	}
+	return (0);
+}
