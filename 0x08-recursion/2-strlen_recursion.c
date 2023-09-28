06f650ee23abc6d7547c5e3857808462a89be25d
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-09-28 10:56:37 -0700

    edit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,12 @@
  *
  *Return: String length
 */
-int x = 0;
+
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 	return (0);
 	}
-	x++;
-	_strlen_recursion(s+1);
-	return (x);
+	return(_strlen_recursion(s+1)+1);
 }
