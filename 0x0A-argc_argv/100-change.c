9241b0e729f38a6cc2ee779703723555432882e1
ShehapAltahawy <shehapkhalil62@gamil.com>
2023-10-02 03:55:19 -0700

    edit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,44 +11,47 @@
 
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
+	int cents, coins = 0;
 
-if (argc != 2)
-{
-	printf("Error\n");
-	return (1);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			coins++;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			coins++;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			coins++;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			coins++;
+			continue;
+		}
+		if ((cents - 1) >= 0)
+		{
+			cents--;
+			coins++;
+		}
+	}
+	printf("%d\n", coins);
+	return (0);
 }
-cents = atoi(argv[1]);
-while (cents > 0)
-{
-	coins++;
-	if ((cents - 25) >= 0)
-	{
-		cents -= 25;
-		coins++;
-		continue;
-	}
-	if ((cents - 10) >= 0)
-	i{
-		cents -= 10;
-		coins++;
-		continue;
-	}
-	if ((cents - 5) >= 0)
-	{
-		cents -= 5;
-		coins++;
-		continue;
-	}
-	if ((cents - 2) >= 0)
-	{
-		cents -= 2;
-		coins++;
-		continue;
-	}
-	cents--;
-	coins++
-}
-printf("%d\n", coins);
-return (0);
-
