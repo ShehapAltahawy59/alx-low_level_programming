a41302e36ae4c69f6a18ebf797a69a34889c2b0d
shehap altahawy <shehapkhalil62@gmail.com>
2024-01-11 13:48:13 -0800

    edit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,19 +8,21 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int counter;
 
-	new = h;
-	counter = 0;
+	int counter = 0;
+
 	if (h == NULL)
-	{
 		return (counter);
-	}
-	while (new != NULL)
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		printf("%d\n",new->n);
-		new = new->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		counter++;
 	}
+
 	return (counter);
 }
