0cf22e6d110233d6b7dd54dd077352262fd3b8c7
shehap62 <shehapkhalil62@gmail.com>
2024-02-02 21:23:11 +0200

    edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			return (0);
 		}
-		index = key_index(key, ht->size);
+		index = key_index((const unsigned char *)key, ht->size);
 		new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 		if (new_node == NULL)
 		{
