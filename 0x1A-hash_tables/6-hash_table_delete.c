c6a36b506787065d5af852b42fbd4bda691f9ea0
shehap62 <shehapkhalil62@gmail.com>
2024-02-02 21:30:09 +0200

    edit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -14,9 +14,9 @@ void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *previous_node;
 	unsigned long int index;
+
 	if (ht == NULL)
 		return;
-	
 	for (index = 0; index < ht->size; index++ )
 	{
 		if (ht->array[index] != NULL && ht->array[index]->next == NULL)
